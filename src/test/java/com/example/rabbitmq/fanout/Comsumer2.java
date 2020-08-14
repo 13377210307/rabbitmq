638cf26509cfb0598961aafe913afeb5acea9e71
wen <2461838916@qq.com>
2020-08-14 14:13:31 +0800

    广播模式

---
+++ src/test/java/com/example/rabbitmq/fanout/Comsumer2.java
@@ -0,0 +1,35 @@
+package com.example.rabbitmq.fanout;
+
+import com.example.rabbitmq.utils.ConnectUtil;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+public class Comsumer2 {
+
+    public static void main(String[] args) throws Exception {
+
+        String exchangeName = "exchange";
+        // 连接通道
+        Channel channel = ConnectUtil.connectRabbit();
+
+        // 声明交换机
+        channel.exchangeDeclare(exchangeName,"fanout");
+
+        //创建临时队列
+        String queue = channel.queueDeclare().getQueue();
+
+        channel.queueBind(queue,exchangeName,"");
+
+        // 消费消息
+        channel.basicConsume(queue,true,new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者2："+new String(body));
+            }
+        });
+    }
+}
--- src/test/java/com/example/rabbitmq/fanout/Provide.java
+++ src/test/java/com/example/rabbitmq/fanout/Provide.java
@@ -1,4 +1,30 @@
 package com.example.rabbitmq.fanout;
 
+import com.example.rabbitmq.utils.ConnectUtil;
+import com.rabbitmq.client.Channel;
+
+/**
+ * 可以有多个消费者
+ * 每个消费者有自己的队列
+ * 每个队列都要绑定到交换机
+ * 生产者发送的消息智能发送到交换机，交换机决定发给哪个队列，生产者无法决定
+ * 交换机把消息发送给绑定过的所有队列
+ * 队列的消费者都能拿到消息，实现一条消息被多个消费者消费
+ */
 public class Provide {
+
+    public static void main(String[] args) throws Exception {
+
+        // 连接通道
+        Channel channel = ConnectUtil.connectRabbit();
+
+        String exchangeName = "exchange";
+
+        // 声明交换机：参数一：交换机名称，参数二：交换模式
+        channel.exchangeDeclare(exchangeName,"fanout");
+
+        // 生产消息
+        channel.basicPublish(exchangeName,"",null,"fanout 模式".getBytes());
+
+    }
 }
